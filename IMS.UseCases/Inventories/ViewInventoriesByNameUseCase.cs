40a85624876fefae02f20c409da76175417cd867
KennyWouters <kenny.wouters2@gmail.com>
2025-07-12 01:31:35 +0200

    Initial commit

---
+++ IMS.UseCases/Inventories/ViewInventoriesByNameUseCase.cs
@@ -0,0 +1,15 @@
+using IMS.CoreBusiness;
+using IMS.UseCases.Interfaces;
+using IMS.UseCases.PluginInterfaces;
+
+namespace IMS.UseCases.Inventories;
+
+public class ViewInventoriesByNameUseCase(IInventoryRepository inventoryRepository) : IViewInventoriesByNameUseCase
+{
+    public async Task<IEnumerable<Inventory>> ExecuteAsync(string name = "")
+    {
+        return await inventoryRepository.GetInventoriesByNameAsync(name);
+        
+    }
+    
+}
