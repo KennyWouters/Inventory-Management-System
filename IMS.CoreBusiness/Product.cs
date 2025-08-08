bd8bce9565804b17ee04bdb1ff78d9e6dc5e96f5
Kenny-Wouters <kendev007@outlook.com>
2025-08-08 21:07:24 +0200

    Enhance product inventory management features
    Updated the `Product` class to manage inventories with methods for adding and removing `ProductInventory` objects. Modified `SearchProductInventoriesComponent.razor` to include an event callback for inventory selection and improved UI for search results. Replaced `SearchProductInventoriesComponent` with `ProductInventoriesComponent` in `AddProduct.razor` for better inventory display and management. Introduced a new `ProductInventory` class to encapsulate product-inventory relationships. Added a CSS file for styling search results.

--- IMS.CoreBusiness/Product.cs
+++ IMS.CoreBusiness/Product.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using IMS.CoreBusiness;
+using System.ComponentModel.DataAnnotations;
 
 public class Product
 {
@@ -13,4 +14,30 @@ public class Product
 
     [Range(0, int.MaxValue, ErrorMessage = "Price must be greater or equal to 0.")]
     public double Price { get; set; }
+
+    public List<ProductInventory> ProductInventories { get; set; } = new List<ProductInventory>();
+
+    public void AddInventory(Inventory inventory)
+    {
+        if (!this.ProductInventories.Any(x => x.Inventory is not null &&
+        x.Inventory.InventoryName.Equals(inventory.InventoryName)))
+        {
+
+            this.ProductInventories.Add(new ProductInventory
+            {
+                InventoryId = inventory.InventoryId,
+                Inventory = inventory,
+                InventoryQuantity = 1,
+                ProductId = this.ProductId,
+                Product = this
+            });
+        }
+    }
+
+    public void RemoveInventory(ProductInventory productInventory)
+    {
+
+        this.ProductInventories?.Remove(productInventory);
+
+    }
 }
